e8ab2cc9a26850f0575e35377b0bc4a3b3e3cee1
RichBurman <>
2023-08-01 14:53:45 +0000

    Create TripList View and urls.py in Football_travel and link to base.html and index.html

--- Football_travel/views.py
+++ Football_travel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Trip
 
 # Create your views here.
+
+
+class TripList(generic.ListView):
+    model = Trip
+    template_name = 'index.html'
