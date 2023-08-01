c63f2ebb5f13cd01a0500d40df71ffd30cdb5031
RichBurman <>
2023-08-01 12:17:50 +0000

    Change digits on Trip Model to 2

--- Football_travel/models.py
+++ Football_travel/models.py
@@ -6,12 +6,10 @@ from django.db import models
 class Trip(models.Model):
     trip_name = models.CharField(max_length=50)
     trip_id = models.AutoField(primary_key=True)
-    seats = models.DecimalField(decimal_places=0, max_digits=1)
-    remaining_seats = models.DecimalField(decimal_places=0, max_digits=1)
+    seats = models.DecimalField(decimal_places=0, max_digits=2)
+    remaining_seats = models.DecimalField(decimal_places=0, max_digits=2)
     price = models.DecimalField(decimal_places=2, max_digits=6)
     date = models.DateField()
 
     def __str__(self):
         return "{self.trip_id} {trip_name}"
-
-        
