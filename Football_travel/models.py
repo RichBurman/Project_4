bc1a2835e485aa2fdcc7027428e7f09c4cb7888d
RichBurman <>
2023-08-02 10:13:51 +0000

    Add Booking Model and link to admin.py

--- Football_travel/models.py
+++ Football_travel/models.py
@@ -13,3 +13,18 @@ class Trip(models.Model):
 
     def __str__(self):
         return f"Name: {self.trip_name} ID Number: {self.trip_id}"
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    userid = models.DecimalField(decimal_places=0, max_digits=3)
+    trip_id = models.DecimalField(decimal_places=0, max_digits=3)
+    trip_name = models.CharField(max_length=50)
+    seats = models.DecimalField(decimal_places=0, max_digits=2)
+    remaining_seats =models.DecimalField(decimal_places=2, max_digits=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"Name: {self.trip_name} ID Number: {self.trip_id} Email: {self.email}"
