ff3e355ef03fdd7e5d8794760da03bc06fd820da
RichBurman <>
2023-08-02 13:40:27 +0000

    Edit Booking Model

--- Football_travel/models.py
+++ Football_travel/models.py
@@ -18,15 +18,11 @@ class Trip(models.Model):
 
 
 class Booking(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    userid = models.DecimalField(decimal_places=0, max_digits=3)
-    trip_id = models.DecimalField(decimal_places=0, max_digits=3)
-    trip_name = models.CharField(max_length=50)
-    seats = models.DecimalField(decimal_places=0, max_digits=2)
-    remaining_seats = models.DecimalField(decimal_places=0, max_digits=2)
-    price = models.DecimalField(decimal_places=2, max_digits=6)
-    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip_booking = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    seats_required = models.DecimalField(decimal_places=0, max_digits=2)
+    created_on = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True)
 
     def __str__(self):
-        return f"Name: {self.trip_name} ID Number: {self.trip_id} Email: {self.email}"
+        return f'{self.trip_booking}. Booked by {self.user} needing {self.seats_required} seats'
