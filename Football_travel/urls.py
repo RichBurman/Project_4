08e4ba12306d4b1eeeb454bd1e72ebcd7f64d2ff
RichBurman <>
2023-08-03 08:42:12 +0000

    Add eny.py file as moved to a different workspace on codeanywhere

--- Football_travel/urls.py
+++ Football_travel/urls.py
@@ -5,8 +5,8 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.TripList.as_view(), name='home'),
-    path('mybookings.html', views.BookingList.as_view(), name='mybookings'),
-    path('newbooking.html', views.newbooking, name='newbooking'),
+    path('mybookings', views.BookingList.as_view(), name='mybookings'),
+    path('newbooking', views.newbooking, name='newbooking'),
     path('add', views.newbooking, name='addnewbooking'),
     path('edit/<booking_id>', views.editbooking, name='editbooking'),
 
