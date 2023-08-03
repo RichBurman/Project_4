0758807348c7fd2c021e3dc92220b92461b3cfa8
RichBurman <>
2023-08-03 06:30:01 +0000

    Add newbooking link to my bookings html

--- Football_travel/urls.py
+++ Football_travel/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.TripList.as_view(), name='home'),
     path('mybookings.html', views.BookingList.as_view(), name='mybookings'),
     path('newbooking.html', views.newbooking, name='newbooking'),
-    path('add', views.newbooking, name='addnewbooking' )
+    path('add', views.newbooking, name='addnewbooking'),
+    path('edit/<booking_id>', views.editbooking, name='editbooking'),
+
+
 ]
