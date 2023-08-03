8b2f627bd1aa2bd80672095c755eca92d8a1edf1
RichBurman <>
2023-08-03 14:55:33 +0000

    Add seats_required to my booking page

--- Football_travel/forms.py
+++ Football_travel/forms.py
@@ -6,4 +6,4 @@ from django.contrib.auth.models import User
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['date', 'comment', 'trip_booked',]
+        fields = ['date', 'comment', 'trip_booked', 'seats_required']
