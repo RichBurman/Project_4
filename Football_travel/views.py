48e717f41af21bc7bb1da15feb7d60dd731a2ec3
RichBurman <>
2023-08-04 14:10:49 +0000

    Add Static folder and style.css

--- Football_travel/views.py
+++ Football_travel/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.views import generic
 from .models import Trip, Booking
 from .forms import BookingForm
