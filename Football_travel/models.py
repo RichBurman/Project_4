4e4517f5962e014285ffb9a9978bac86f596797f
RichBurman <>
2023-08-06 11:48:18 +0000

    Change remaining_seats in Trip Model to PositiveIntegerField with max value of 80

--- Football_travel/models.py
+++ Football_travel/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from datetime import date
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 
@@ -10,21 +11,21 @@ class Trip(models.Model):
     trip_name = models.CharField(max_length=50)
     trip_id = models.AutoField(primary_key=True)
     seats = models.IntegerField(default=80)
-    remaining_seats = models.IntegerField()
-    price = models.IntegerField()
-    date = models.DateField()
+    remaining_seats = models.PositiveIntegerField(validators=[MaxValueValidator(80)])
+    price=models.IntegerField()
+    date=models.DateField()
 
     def __str__(self):
         return f"Name: {self.trip_name} ID Number: {self.trip_id}"
 
 
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(blank=False, default=date.today)
-    created_on = models.DateTimeField(auto_now_add=True)
-    comment = models.TextField(blank=True)
-    trip_booked = models.ForeignKey(Trip, on_delete=models.CASCADE)
-    seats_required = models.IntegerField()
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    date=models.DateField(blank=False, default=date.today)
+    created_on=models.DateTimeField(auto_now_add=True)
+    comment=models.TextField(blank=True)
+    trip_booked=models.ForeignKey(Trip, on_delete=models.CASCADE)
+    seats_required=models.IntegerField()
 
     def __str__(self):
         return f"User: {self.user} booked {self.trip_booked} on {self.created_on}"
