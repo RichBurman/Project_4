75d466709a55373a741a14553c16004c41a3e580
RichBurman <>
2023-08-08 10:11:34 +0000

    Edit Booking Form so now name of trip is showed to the user rather than Name and Id

--- Football_travel/forms.py
+++ Football_travel/forms.py
@@ -4,9 +4,17 @@ from django.contrib.auth.models import User
 
 
 class BookingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['trip_booked'].queryset = Trip.objects.all()
+        self.fields['trip_booked'].label_from_instance = lambda obj: obj.trip_name
+
+
     class Meta:
         model = Booking
-        fields = ['date', 'trip_booked', 'seats_required', 'comment']
+        fields = ['date', 'trip_booked',
+                  'seats_required', 'comment']
         labels = {
             'date': 'Date of Booking',
             'trip_booked': 'Selected Trip',
